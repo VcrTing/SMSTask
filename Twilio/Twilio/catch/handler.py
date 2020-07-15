aaf94355c6163a072b760aaa665b516ff3c384be
System Administrator <root@ZengYingQiongs-Mac-mini.local>
2020-07-15 17:31:50 +0800

    2.15.7

--- Twilio/Twilio/catch/handler.py
+++ Twilio/Twilio/catch/handler.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render, redirect
 
-def unfound(request):
-    return render(request, 'catch/404.html')
+def unfound(request, exception, template_name='catch/404.html'):
+    return render(request, template_name)
 
-def unright(request):
-    return render(request, 'catch/500.html')
+def unright(request, exception, template_name='catch/500.html'):
+    return render(request, template_name)
 
-def unrequest(request):
-    return render(request, 'catch/400.html')
+def unrequest(request, exception, template_name='catch/400.html'):
+    return render(request, template_name)
