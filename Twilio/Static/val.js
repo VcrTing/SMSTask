3fe4757c19d76d5f6363f01ed05c6c87db989512
System Administrator <root@ZengYingQiongs-Mac-mini.local>
2020-08-04 17:02:50 +0800

    2.18.0

--- Twilio/Static/val.js
+++ Twilio/Static/val.js
@@ -1,20 +1,20 @@
-// 验证 電郵內容
+// 驗證 電郵內容
 _valide_message = function(message) {
     if (!message | message == undefined | message == '') {
         return '電郵內容不為空'
     } else {
         const len = message.length
         if (len < 20) {
-            return '電郵內容字数应当大于20'
+            return '電郵內容字數應當大於20'
         } else if (len > 7200) {
-            return '電郵內容字数不得大于 7200'
+            return '電郵內容字數不得大於 7200'
         }
     }
     return true
 
 }
 
-// 验证一般文本框
+// 驗證壹般文本框
 _valide_text = function(text, msg_named, isEmpty = false, min_len = 4, max_len = 240) {
     if (!isEmpty) {
         if (!text | text == undefined | text == '') {
@@ -23,29 +23,29 @@ _valide_text = function(text, msg_named, isEmpty = false, min_len = 4, max_len =
     } else {
         const len = text.length
         if (len < 4) {
-            return msg_named + '字数应当大于' + min_len
+            return msg_named + '字數應當大於' + min_len
         } else if (len > max_len) {
-            return msg_named + '字数不得大于' + max_len
+            return msg_named + '字數不得大於' + max_len
         }
     }
     return true
 }
 
-// 验证名字
+// 驗證名字
 _valide_named = function(named) {
     if (!named | named == undefined | named == '') {
         return '名字不為空'
     }  else {
         const len = named.length
         if (len < 2) {
-            return '确保名字字数大于2'
+            return '確保名字字數大於2'
         } else if (len > 40) {
-            return '名字字数不得大于40'
+            return '名字字數不得大於40'
         }
 
         let char = /[`~!@#$%^&*(“”‘’'、～·！—_+|=;？，。\\)<>?:"{},.\/;'[\]]/;
         if (char.test(named)) {
-            return '名字中不应有特殊字符！！！'
+            return '名字中不應有特殊字符！！！'
         }
     }
     return true
@@ -74,7 +74,7 @@ _valide_phone = function(phone) {
     return true
 }
 
-// 验证时间 年月日
+// 驗證時間 年月日
 _valide_date = function(dates, is_split = true) {
     if (is_split) {
         dates = dates.split('-')
@@ -82,11 +82,11 @@ _valide_date = function(dates, is_split = true) {
 
     if (dates.length == 3) {
         if (dates[0].length != 4) {
-            return '年份格式错误，年份应为4位数！'
+            return '年份格式錯誤，年份應為4位數！'
         } else if ((dates[1].length > 2) || (dates[1].length = 0)) {
-            return '月份格式错误，月份应为2两位数！'
+            return '月份格式錯誤，月份應為2兩位數！'
         } else if ((dates[2].length > 2)  || (dates[2].length = 0)) {
-            return '日期格式错误，日期应为2两位数！'
+            return '日期格式錯誤，日期應為2兩位數！'
         }
 
         if (dates[1] == '' ) {
@@ -96,12 +96,12 @@ _valide_date = function(dates, is_split = true) {
         }
 
         if ((parseInt(dates[1]) > 12 ) || (parseInt(dates[1]) == 0)) {
-            return '月份数值错误！'
+            return '月份數值錯誤！'
         } else if ((parseInt(dates[2]) > 31) || (parseInt(dates[2]) == 0)) {
-            return '日期数值错误'
+            return '日期數值錯誤'
         }
     } else {
-        return '格式错误，格式为：年-月-日。'
+        return '格式錯誤，格式為：年-月-日。'
     }
     return true
 }
@@ -109,7 +109,7 @@ _valide_date = function(dates, is_split = true) {
 _valide_bith = function(date, isEmpty) {
     if (!isEmpty) {
         if (!date | date == undefined | date == '') {
-            return '生日的时间不能為空'
+            return '生日的時間不能為空'
         }
     }
     let res = _valide_date(date)
@@ -119,7 +119,7 @@ _valide_bith = function(date, isEmpty) {
     return '生日' + res
 }
 
-// 验证邮箱
+// 驗證郵箱
 _val_email = function(date, isEmpty = false) {
     
     if (!isEmpty) {
@@ -135,7 +135,7 @@ _val_email = function(date, isEmpty = false) {
     return true
 }
 
-// 验证 标签
+// 驗證 標簽
 _val_tag = function(named) {
     if ((named == '') || (named == null) || (named == undefined)) {
         return '標簽名稱不為空'
