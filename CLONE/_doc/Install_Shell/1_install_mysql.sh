f57e8f7c5dea29a62ba70005ec8ebe108ca02186
System Administrator <root@ZengYingQiongs-Mac-mini.local>
2021-06-28 16:54:47 +0800

    3.3.4

--- CLONE/_doc/Install_Shell/1_install_mysql.sh
+++ CLONE/_doc/Install_Shell/1_install_mysql.sh
@@ -2,6 +2,7 @@ echo 'START INSTALL'
 echo '======================>'
 echo ''
 echo ''
+mysql_package='mysql80-community-release-el7-3.noarch.rpm'
 ## MYSQL
 echo '准备安装 Mysql'
 older_db=$(rpm -qa | grep mariadb)
@@ -10,8 +11,8 @@ unset $older_db
 yum update -y
 ### 开始下载，然后安装
 echo '安装 Mysql'
-wget https://dev.mysql.com/get/mysql80-community-release-el7-3.noarch.rpm
-rpm -Uvh https://dev.mysql.com/get/mysql80-community-release-el7-3.noarch.rpm
+wget https://dev.mysql.com/get/$mysql_package
+rpm -Uvh https://dev.mysql.com/get/$mysql_package
 yum install -y mysql-community-server
 service mysqld start
 ### 查看状态，准备登录
@@ -21,7 +22,10 @@ password=$(grep 'temporary password' /var/log/mysqld.log)
 echo ''
 echo '当前安装的MYSQL的备用密码为：'
 echo $password
+now_path=$(pwd)
+rm -rf ./$mysql_package
 echo ''
 echo '<======================'
 unset $password
+unset $mysql_package
 ## 结束
--- CLONE/_doc/Install_Shell/2_change_mysql_password.sh
+++ CLONE/_doc/Install_Shell/2_change_mysql_password.sh
@@ -6,13 +6,15 @@ echo 'MYSQL 安装 完成。'
 password=$(grep 'temporary password' /var/log/mysqld.log)
 echo '当前安装的MYSQL的备用密码为：'
 echo $password
-### 登录 mysql
-mysql -u root -p $password <<EOF
-alter user 'root'@'localhost' identified by 'VcrTing.ZT123zlt';
-grant all privileges on *.* to 'root'@'%';
-FLUSH PRIVILEGES;
-exit;
-EOF
+read -t 60 -p "请输入该备用密码:" password
+### 书写 mysql 命令
+change_pwd="alter user 'root'@'localhost' identified by 'VcrTing.ZT123zlt';"
+change_role="grant all privileges on *.* to 'root'@'%';"
+refresh="FLUSH PRIVILEGES;"
+### 执行命令
+mysql -uroot -p$password -e $change_pwd --connect-expired-password
+mysql -uroot -p$password -e $change_role --connect-expired-password
+mysql -uroot -p$password -e $refresh --connect-expired-password
 echo 'MYSQL 操作完成'
 echo ''
 echo 'MYSQL新改的密码为:'
