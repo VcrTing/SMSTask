12529fe12c254b2eccef03e8f80660de71348de0
System Administrator <root@ZengYingQiongs-Mac-mini.local>
2021-06-29 19:00:31 +0800

    3.3.83

--- CLONE/Twilio_2/Appis/User/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class UserConfig(AppConfig):
-    name = 'User'
-    verbose_name = '后台员工'
--- CLONE/_doc/Install_Shell/8_update_data.sh
+++
@@ -1,9 +0,0 @@
-echo '======================>'
-echo ''
-echo ''
-### Python
-echo '准备安装 数据 Migrate'
-cd /root/SMSTask/CLONE/Twilio_1
-python3 manage.py migrate
-echo '完成'
-echo '<======================'
